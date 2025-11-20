7d5b062d95307d90daccc99994fab2fa5a48ad50
Eine0322 <eine113589@gmail.com>
2025-11-20 20:25:15 +0900

    feat : 로딩 페이지 구현 완료

--- src/components/Icon/IconMap.js
+++ src/components/Icon/IconMap.js
@@ -1,5 +1,13 @@
+import graphGood from '../../assets/graph/graph-good.svg?react'
+import graphNormal from '../../assets/graph/graph-normal.svg?react'
+import graphBad from '../../assets/graph/graph-bad.svg?react'
+import logoBackgroundLight from '../../assets/logo/logo-background-light.svg?react'
 import topbarButtonPrev from '../../assets/topbar/topbar-button-prev.svg?react'
 
 export const iconMap = {
+  'graph-good': graphGood,
+  'graph-normal': graphNormal,
+  'graph-bad': graphBad,
+  'logo-background-light': logoBackgroundLight,
   'topbar-button-prev': topbarButtonPrev,
 }
---
+++ src/pages/loading/entry/LoadingPage.jsx
@@ -0,0 +1,39 @@
+import { Icon } from '../../../components/Icon/Icon'
+import './LoadingPage.css'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function LoadingPage() {
+  //자동으로 넘어가기
+  const LOADING_DELAY = 2000
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate(`/result`)
+    }, LOADING_DELAY)
+
+    return () => clearTimeout(timer)
+  }, [navigate])
+
+  return (
+    <div className='loading__container'>
+      <div className='loading__loader--container'>
+        <div class='loading__loader'></div>
+      </div>
+      <div className='loading__text'>
+        <p className='loading__text--AI'>AI가 분석하고 있어요</p>
+        <p className='loading__text--wait'>잠시만 기다려주세요!</p>
+      </div>
+      <Icon
+        name='logo-background-light'
+        width={630}
+        height={630}
+        className='loading__container loading__background--logo'
+      />
+    </div>
+  )
+}
+
+export default LoadingPage
